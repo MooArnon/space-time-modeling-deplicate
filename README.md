f7ac0cbaf4c4bf04fad0b5a7d9c32652a81d842d
MooArnon <oomarnon.000@gmail.com>
2023-07-05 08:06:55 +0700

    docs: add external README.d

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # space-time-modeling
-For time series modeling.
+I developed the `space_time_modeling` for the regression problems. <br>
+You guys can perform the modeling by following the instruction at `run_local.py`, <br>
+or, just use that code to run it.
+
+PS. I always update the feature of this package. In the future, it can be run without the manual assignation, just use one function.
+
+Arnon,
+arnon.phongsiang@gmail.com
 
 
 ```project_directory/
@@ -8,6 +15,10 @@ For time series modeling.
 │   |── preprocess.py
 |   |   |──__init__.py
 |   |   |──...
+|   |── modeling.py
+|   |   |──__init__.py
+|   |   |──...
 └── tests/
-    └── test.py
+    └── test_modeling.py
+    └── test_preprocess.py
 ```
--- run_local.py
+++ run_local.py
@@ -14,22 +14,26 @@ from space_time_modeling.resources.deep_model import NNModel
 #----------------------------------------------------------------------------#
 WINDOW_SIZE = 10
 
+# Read data
 df = pd.read_csv(
     os.path.join("tests", "BTC-USD.csv")
 )
 
+# Get preprocessing engine
 prep = get_preprocess_engine(
     column="Open", 
     window_size=WINDOW_SIZE,
     diff=False,
 )
 
+# Calculate x and y
 x, y = prep.process(df=df)
 
 
 #----------#
 # Modeling #
 #----------------------------------------------------------------------------#
+# Simple NN model
 model_nn = NNModel(
     input_size=WINDOW_SIZE, 
     hidden_size=1024, 
@@ -37,8 +41,10 @@ model_nn = NNModel(
     redundance=1
 )
 
+# Get modeling engine
 engine = DeepModeling(model_nn)
 
+# Train it
 engine.train(
     x, y, 
     train_kwargs={"lr": 5e-5, "epochs":500}, 
