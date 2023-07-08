624239f69c35e7c785a230eb35e4f4067f652260
MooArnon <oomarnon.000@gmail.com>
2023-07-08 21:07:36 +0700

    feat: make installable package

--- space_time_modeling/__init__.py
+++ space_time_modeling/__init__.py
@@ -4,3 +4,13 @@
 
 from space_time_modeling.preprocess import get_preprocess_engine
 from space_time_modeling.modeling import get_model_engine, DeepModeling
+
+#------------#
+# Properties #
+#----------------------------------------------------------------------------#
+__version__ = 0.1
+__author__ = "Arnon Phongsiang" 
+__email__ = "arnon.phongsiang@gmail.com"
+__github__ = "https://github.com/MooArnon"
+__medium__ = "https://medium.com/@oomarnon"
+__linkedin__ = "https://www.linkedin.com/in/arnon-pongsiang-320796214/"
