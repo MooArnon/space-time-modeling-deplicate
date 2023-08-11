9d6e204af0c7d3c40d6a49351b337b17636ba023
MooArnon <oomarnon.000@gmail.com>
2023-08-11 07:34:14 +0700

    feat: structure of model as json

--- run_local.py
+++ run_local.py
@@ -12,7 +12,7 @@ from space_time_modeling import get_model_engine
 # Variable #
 #----------------------------------------------------------------------------#
 
-WINDOW_SIZE = 15
+WINDOW_SIZE = 10
 
 #--------------#
 # Process data #
@@ -20,14 +20,14 @@ WINDOW_SIZE = 15
 
 # Read data
 df = pd.read_csv(
-    os.path.join("tests", "BTC-USD.csv")
+    os.path.join("data", "BTC-Hourly.csv")
 )
 
 print(df.shape)
 
 # Get preprocessing engine
 prep = get_preprocess_engine(
-    column="Open", 
+    column="open", 
     window_size=WINDOW_SIZE,
     diff=False,
     engine="series"
@@ -47,7 +47,9 @@ model_engine = get_model_engine(
     engine="deep",
     architecture = "n-beats",
     input_size = WINDOW_SIZE,
-    hidden_size = 512
+    hidden_size = 128,
+    num_stacks = 8,
+    num_blocks = 16
 )
 
 #----------------------------------------------------------------------------#
@@ -59,7 +61,7 @@ model_engine.modeling(
     y, 
     result_name = "n-beats",
     epochs=100,
-    train_kwargs={"lr": 3e-5},
+    train_kwargs={"lr": 3e-7},
     test_ratio = 0.15,
 )
 
